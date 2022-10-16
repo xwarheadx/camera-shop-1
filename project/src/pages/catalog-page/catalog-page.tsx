8b6b6dc169b866188f745801811b30ae5970c104
xwarheadx <sergeypost89@gmail.com>
2022-10-16 18:51:22 +0300

    Создание компонента Баннер

--- project/src/pages/catalog-page/catalog-page.tsx
+++ project/src/pages/catalog-page/catalog-page.tsx
@@ -1,5 +1,6 @@
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
+import Banner from '../../components/banner/banner';
 
 export default function CatalogPage(): JSX.Element {
   return (
@@ -86,13 +87,7 @@ export default function CatalogPage(): JSX.Element {
         <Header/>
 
         <main>
-          <div className="banner">
-            <picture>
-              <source type="image/webp" srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"/>
-              <img src="img/content/banner-bg.jpg" srcSet="img/content/banner-bg@2x.jpg 2x" width="1280" height="280" alt="баннер"/>
-            </picture>
-            <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="!">Подробнее</a></p>
-          </div>
+          <Banner />
           <div className="page-content">
             <div className="breadcrumbs">
               <div className="container">
